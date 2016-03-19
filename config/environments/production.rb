7e3b52d60c162510c1a5f0e203e3d9f9ed696dd7
Kyle Leeyaw <kyle.leeyaw@gamil.com>
2016-03-19 12:11:52 -0400

    implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for heroku
+  #Set this to actual host
+  config.action_mailer.default_url_options = { :host 'bitnote.ca'}
 end
